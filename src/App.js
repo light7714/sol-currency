5a304e7400519834fe9516abb334d5f62757e7cc
Shubham Vermani <svermani_be19@thapar.edu>
2022-01-16 21:07:02 +0530

    Connection to wallet and airdrop working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+	Connection,
+	clusterApiUrl,
+	PublicKey,
+	LAMPORTS_PER_SOL,
+	KeyPair,
+	Transaction,
+	sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+
+const App = () => {
+	const [walletConnected, setWalletConnected] = useState(false);
+	const [provider, setProvider] = useState();
+	const [loading, setLoading] = useState();
+
+	const getProvider = async () => {
+		// Due to installing @solana/web3.js , we now have access to the “solana” variable globally in the window.
+		if ('solana' in window) {
+			const provider = window.solana;
+			if (provider.isPhantom) {
+				return provider;
+			}
+		} else {
+			window.open('https://www.phantom.app/', '_blank');
+		}
+	};
+
+	const walletConnectionHelper = async () => {
+		if (walletConnected) {
+			//Disconnect Wallet
+			setProvider();
+			setWalletConnected(false);
+		} else {
+			const userWallet = await getProvider();
+			if (userWallet) {
+				await userWallet.connect();
+				userWallet.on('connect', async () => {
+					setProvider(userWallet);
+					setWalletConnected(true);
+				});
+			}
+		}
+	};
+
+	const airDropHelper = async () => {
+		try {
+			setLoading(true);
+			const connection = new Connection(
+				clusterApiUrl('devnet'),
+				'confirmed'
+			);
+			const fromAirDropSignature = await connection.requestAirdrop(
+				new PublicKey(provider.publicKey),
+				LAMPORTS_PER_SOL
+			);
+			await connection.confirmTransaction(fromAirDropSignature, {
+				commitment: 'confirmed',
+			});
+
+			console.log(
+				`1 SOL airdropped to your wallet ${provider.publicKey.toString()} successfully`
+			);
+			setLoading(false);
+		} catch (err) {
+			console.log(err);
+			setLoading(false);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Create your own token using JavaScript</h1>
+
+			{walletConnected ? (
+				<>
+					<p>
+						<strong>Public Key:</strong>{' '}
+						{provider.publicKey.toString()}
+					</p>
+
+					<p>
+						Airdrop 1 SOL into your wallet
+						<button disabled={loading} onClick={airDropHelper}>
+							AirDrop SOL{' '}
+						</button>
+					</p>
+
+          
+				</>
+			) : (
+				<></>
+			)}
+
+			<button onClick={walletConnectionHelper} disabled={loading}>
+				{!walletConnected ? 'Connect Wallet' : 'Disconnect Wallet'}
+			</button>
+		</div>
+	);
+};
 
 export default App;
